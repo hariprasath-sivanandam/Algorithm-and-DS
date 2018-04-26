88d9827189b4e2f9385cad6f88408100d6f49e6e
hariprasath <sivanandam.h@husky.neu.edu>
2018-04-25 23:43:55 -0400

    solved jumpgame problem

---
+++ array/jumpagame.java
@@ -0,0 +1,28 @@
+Question:
+
+https://leetcode.com/problems/jump-game/description/
+
+55. Jump Game
+Given an array of non-negative integers, you are initially positioned at the first index of the array.
+Each element in the array represents your maximum jump length at that position.
+Determine if you are able to reach the last index.
+
+Example 1:
+
+Input: [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+Solution:
+
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}
