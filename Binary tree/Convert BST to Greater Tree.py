52faf49588053f6dd89e7495d8e08e386c3a7af7
hariprasath <sivanandam.h@husky.neu.edu>
2018-06-06 23:56:35 -0400

    Convert BST to Greater Tree.py

---
+++ Binary tree/Convert BST to Greater Tree.py
@@ -0,0 +1,41 @@
+"""
+https://leetcode.com/problems/convert-bst-to-greater-tree/description/
+538. Convert BST to Greater Tree
+Given a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus sum of all keys greater than the original key in BST.
+
+Example:
+
+Input: The root of a Binary Search Tree like this:
+              5
+            /   \
+           2     13
+
+Output: The root of a Greater Tree like this:
+             18
+            /   \
+          20     13
+
+Solution:
+"""
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        self.sum=0
+        def change(root):
+            if root:
+                change(root.right)
+                root.val+=self.sum
+                self.sum=root.val
+                change(root.left)
+                return root
+        return change(root)
